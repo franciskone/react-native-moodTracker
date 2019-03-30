ea364c7a5051d273e58f8cc135ca6d384eee5f89
franciskone <francesco.cappelli.developer@gmail.com>
2019-03-30 21:56:00 +0000

    added eslint config

--- moodTrackerApp/src/components/MoodPicker/MoodFace.jsx
+++ moodTrackerApp/src/components/MoodPicker/MoodFace.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import {
-  Image, StyleSheet
-} from 'react-native';
+import { Image, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
 
 import { getColorByMoodLevel } from '../../util';
